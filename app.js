949a852c52fc950546401ab915e9a07ee5e2bdc4
H2O-OO <oues.hamdy@gmail.com>
2025-02-19 17:03:09 +0100

    installing express and redring some content on the web page

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 3003;
+
+const server = app.listen(port, () => {
+  console.log(`server is listening on port ${port}`);
+});
+
+app.get("/", (req, res, next) => {
+  res.status(200).send("Yahoo");
+});
