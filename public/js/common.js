d72b3b8442096a8e58213174d5bdc21748243c86
H2O-OO <oues.hamdy@gmail.com>
2025-02-22 16:32:02 +0100

    Outputting posts onto the newsfeed(home)

--- public/js/common.js
+++ public/js/common.js
@@ -29,8 +29,12 @@ $("#submitPostButton").click((event) => {
 
 function createPostHtml(postData) {
   const postedBy = postData.postedBy;
+
+  if (postedBy._id === undefined) {
+    return console.log("user object not populated");
+  }
   const displayName = postedBy.firstName + "" + postedBy.lastName;
-  const timeStamp = postedBy.createdAt;
+  let timeStamp = timeDifference(new Date(), new Date(postData.createdAt));
   return `<div class='post'>
      <div class='mainContentContainer'>
        <div class='userImageContainer'>
@@ -66,4 +70,29 @@ function createPostHtml(postData) {
        </div>
      </div>
   </div>`;
+}
+
+function timeDifference(current, previous) {
+  const msPerMinute = 60 * 1000;
+  const msPerHour = msPerMinute * 60;
+  const msPerDay = msPerHour * 24;
+  const msPerMonth = msPerDay * 30;
+  const msPerYear = msPerDay * 365;
+
+  let elapsed = current - previous;
+
+  if (elapsed < msPerMinute) {
+    if (elapsed / 1000 < 30) return "Just now";
+    return Math.round(elapsed / 1000) + " seconds ago";
+  } else if (elapsed < msPerHour) {
+    return Math.round(elapsed / msPerMinute) + " minutes ago";
+  } else if (elapsed < msPerDay) {
+    return Math.round(elapsed / msPerHour) + " hours ago";
+  } else if (elapsed < msPerMonth) {
+    return Math.round(elapsed / msPerDay) + " days ago";
+  } else if (elapsed < msPerYear) {
+    return Math.round(elapsed / msPerMonth) + " months ago";
+  } else {
+    return Math.round(elapsed / msPerYear) + " years ago";
+  }
 }
---
+++ public/js/home.js
@@ -0,0 +1,15 @@
+$(document).ready(() => {
+  $.get("/api/posts", (results) => {
+    outputPosts(results, $(".postContainer"));
+  });
+});
+function outputPosts(results, container) {
+  container.html("");
+  results.forEach((result) => {
+    const html = createPostHtml(result);
+    container.append(html);
+  });
+  if (results.length == 0) {
+    container.append("<span class='noResults'>Nothing to show.</span>");
+  }
+}
