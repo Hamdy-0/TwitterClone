dd726f7837c97c7f08c4545a6ba76dc297e08189
H2O-OO <oues.hamdy@gmail.com>
2025-03-05 00:59:55 +0100

    adjusting the chat inbox and working on messages style and logique

---
+++ public/js/chatPage.js
@@ -0,0 +1,131 @@
+$(document).ready(() => {
+  $.get(`/api/chats/${chatId}`, (data) => {
+    $("#chatName").text(getChatName(data));
+  });
+  $.get(`/api/chats/${chatId}/messages`, (data) => {
+    let messages = [];
+    let lastSenderId = "";
+    data.forEach((message, index) => {
+      let html = createMessageHtml(message, data[index + 1], lastSenderId);
+      messages.push(html);
+      lastSenderId = message.sender._id;
+    });
+    let mesaagesHtml = messages.join("");
+    addMessagesHtmlToPage(mesaagesHtml);
+    scrollToBottom(false);
+    $(".loadingSpinnerContainer").remove();
+    $(".chatContainer").css("visibility", "visible");
+  });
+});
+
+$("#chatNameButton").click(() => {
+  let name = $("#chatNameTextBox").val().trim();
+
+  $.ajax({
+    url: "/api/chats/" + chatId,
+    type: "PUT",
+    data: { chatName: name },
+    success: (data, status, xhr) => {
+      if (xhr.status != 204) {
+        alert("could not update");
+      } else {
+        location.reload();
+      }
+    },
+  });
+});
+
+$(".sendMessageButton").click(() => {
+  messageSubmitted();
+});
+$(".inputTextbox").keydown((e) => {
+  if (e.which === 13 && !e.shiftKey) {
+    messageSubmitted();
+    return false;
+  }
+});
+function addMessagesHtmlToPage(html) {
+  $(".chatMessages").append(html);
+}
+function messageSubmitted() {
+  let content = $(".inputTextbox").val().trim();
+  if (content != "") {
+    sendMessage(content);
+    $(".inputTextbox").val("");
+  }
+}
+function sendMessage(content) {
+  $.post(
+    "/api/messages",
+    { content: content, chatId: chatId },
+    (data, status, xhr) => {
+      if (xhr.status != 201) {
+        alert("Could not send message");
+        $(".inputTextbox").val(content);
+        return;
+      }
+      addChatMessageHtml(data);
+    }
+  );
+}
+
+function addChatMessageHtml(message) {
+  if (!message || !message._id) {
+    alert("Message is not valid");
+    return;
+  }
+  let messageDiv = createMessageHtml(message, null, "");
+  addMessagesHtmlToPage(messageDiv);
+  scrollToBottom(true);
+}
+
+function createMessageHtml(message, nextMessage, lastSenderId) {
+  let sender = message.sender;
+  let senderName = sender.firstName + " " + sender.lastName;
+  let currentSenderId = sender._id;
+  let nextSenderId = nextMessage != null ? nextMessage.sender._id : "";
+
+  let isFirst = lastSenderId != currentSenderId;
+  let isLast = nextSenderId != currentSenderId;
+  let isMine = message.sender._id == userLoggedIn._id;
+
+  let liClassName = isMine ? "mine" : "theirs";
+
+  let nameElement = "";
+  if (isFirst) {
+    liClassName += " first";
+    if (!isMine) {
+      nameElement = `<span class='senderName'>${senderName}</span>`;
+    }
+  }
+  let profileImage = "";
+  if (isLast) {
+    liClassName += " last";
+    profileImage = `<img src=${sender.profilePic}>`;
+  }
+  let imageContainer = "";
+  if (!isMine) {
+    imageContainer = `<div class='imageContainer'>
+       ${profileImage}
+    </div>`;
+  }
+  return `<li class='message ${liClassName}'>
+  ${imageContainer}
+       <div class='messageContainer'>
+       ${nameElement}
+          <span class='messageBody'>
+          ${message.content}
+          </span>
+       </div>
+    </li>`;
+}
+
+function scrollToBottom(animated) {
+  let container = $(".chatMessages");
+  let scrollHeight = container[0].scrollHeight;
+  if (animated) {
+    container.animate({ scrollTop: scrollHeight }, "slow");
+  } else {
+    container.scrollTop(scrollHeight);
+  }
+}
---
+++ public/js/inboxPage.js
@@ -0,0 +1,59 @@
+$(document).ready(() => {
+  $.get("/api/chats", (data, status, xhr) => {
+    if (xhr.status == 400) {
+      alert("Could not get chat list");
+    } else {
+      outputChatList(data, $(".resultsContainer"));
+    }
+  });
+});
+
+function outputChatList(chatList, container) {
+  chatList.forEach((chat) => {
+    let html = createChatHtml(chat);
+    container.append(html);
+  });
+  if (chatList.length == 0) {
+    container.append('<span class="noResuults">Nothing to show.</span>');
+  }
+}
+function createChatHtml(chatData) {
+  let chatName = getChatName(chatData); /// TO DO
+  let image = getChatImageElements(chatData); // TO DO
+
+  let latestMessage = getLatestMessage(chatData.latestMessage);
+  return `<a href='/messages/${chatData._id}' class='resultListItem'>
+    ${image}
+    <div class='resultsDetailsContainer ellipsis'>
+    <span class='heading ellipsis'>${chatName}</span>
+    <span class='subText ellipsis'>${latestMessage}</span>
+    
+    </div>
+    </a>`;
+}
+
+function getLatestMessage(latestMessage) {
+  if (latestMessage != null) {
+    let sender = latestMessage.sender;
+    return `${sender.firstName} ${sender.lastName}: ${latestMessage.content}`;
+  }
+  return "New Chat";
+}
+
+function getChatImageElements(chatData) {
+  let otherChatUsers = getOtherChatUsers(chatData.users);
+  let groupChatClass = "";
+
+  let chatImage = getUserChatImageElement(otherChatUsers[0]);
+  if (otherChatUsers.length > 1) {
+    groupChatClass = "groupChatImage";
+    chatImage += getUserChatImageElement(otherChatUsers[1]);
+  }
+  return `<div class='resultsImageContainer ${groupChatClass}'>${chatImage}</div>`;
+}
+function getUserChatImageElement(user) {
+  if (!user || !user.profilePic) {
+    return alert("user passed into function is invalid ");
+  }
+  return `<img src='${user.profilePic}' alt='user's profile pic'>`;
+}
